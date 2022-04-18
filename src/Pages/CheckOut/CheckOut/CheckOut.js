a14ab41c8ef60f7c6cf2b5f111c4645ed6ba3c11
mk-hasan <mkhasan6891@gmail.com>
2022-04-18 15:06:10 +0600

    about me page updated

--- src/Pages/CheckOut/CheckOut/CheckOut.js
+++ src/Pages/CheckOut/CheckOut/CheckOut.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const CheckOut = () => {
     return (
-        <div className='container'>
+        <div style={{ height: "100vh" }} className='container'>
             <h3 className='text-center mt-2'>Yes! Now you can buy my course..</h3>
         </div>
     );
