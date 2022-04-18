664e2dc8109f84c86e5769b6db8bb1bc8b611f0d
mk-hasan <mkhasan6891@gmail.com>
2022-04-18 22:14:21 +0600

    about me page updated

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div style={{ height: "100vh" }} className='container'>
-            <h2 className='text-center'>This is About me</h2>
+        <div style={{ height: "100vh" }} className='container mt-2'>
+            <h2 className='text-center text-success'>This is About me</h2>
             <p>
-                I am Md. Kamrul Hasan. Last 4 months I have spend beautiful and sharp path with Programming Hero for build me a web developer. Modern technology of web developing with javascript is provided by PHero, it's helps to become web developer. It's really challenging but not impossible. Every day's workings give me progressed from last days. In future InshaAllah I will also go on this hard work like now. However it is important that honesty in dedication must be reached anyone to his goal.
+                I am Md. Kamrul Hasan. Last 4 months I have spend beautiful and sharp path with Programming Hero for build me a web developer. Modern technology of web developing with javascript is provided by PHero, it's helps to become web developer. It's really challenging but not impossible. Every day's workings give me progressed from last days, sharpen my skills and knowledge. In future InshaAllah I will also go on this hard work like now. I'm eagerly waiting explore myself more in professional web development career in IT Industry. Exploring and taking challenge never feels me give up. However it is important that honesty in dedication must be reached anyone to his goal.
             </p>
         </div>
     );
