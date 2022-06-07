0c5ece2925a17f8261e148cb7395d59b275f6c66
mk-hasan <mkhasan6891@gmail.com>
2022-06-07 13:01:13 +0600

    exprerience section some change for test

--- src/Pages/Home/Experience/Experience.js
+++ src/Pages/Home/Experience/Experience.js
@@ -11,7 +11,7 @@ const Experience = () => {
                 </div>
                 <div className='col-lg-6 d-flex align-items-center justify-content-end'>
                     <div>
-                        <h4 className='fw-bold pb-2 fs-2'>Mentoring Program</h4>
+                        <h4 className='fw-bold pb-2 fs-2'>Mentoring Program Program</h4>
                         <p>My mentorship program helps who are looking forwar this type of system, new to  javascript, or facing a changing academic or UI UX design problems to come to grips with unexpected aspects of through the assistance of other professionals in their field. I am Experienced, expert instructors serve as mentors for less experienced educators, walking them through proven efficient strategies for managing disciplinary problems, time management, parent interaction, and more. I build a custom-made curriculum starting at a level that's comfortable...</p>
                     </div>
                 </div>
